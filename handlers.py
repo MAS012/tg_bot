2218b5a6ab03a532c0e2f9a76b84216f496372c7
MAS012 <mathmat012@gmail.com>
2024-06-04 18:56:00 +0500

    Builder Added

--- handlers.py
+++ handlers.py
@@ -11,8 +11,12 @@ async def start(message: Message):
 
 @router.message(Command('help'))
 async def help_cmd(message: Message):
-    await message.answer("I cannot help you bruh")
+    await message.answer("I cannot help you bruh \n Choose options instead", reply_markup=kb.options)
 
 @router.message(F.text =='What up')
 async def f_reply(message:Message):
-    await message.reply("Nothing much G")
+    await message.reply("Nothing much G \n Choose Priority", reply_markup=await kb.pr())
+
+@router.message(F.text =='Set Reminder')
+async def f_reply(message:Message):
+    await message.reply("Choose Time", reply_markup=kb.date)
--- kb.py
+++ kb.py
@@ -1,8 +1,20 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-
-main = ReplyKeyboardMarkup(keyboard=[[
-    KeyboardButton(text= 'My Reminders')],
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+main = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text= 'Set Reminder')],
     [KeyboardButton(text = 'Deleted')]
 ])
 options = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Banger", url="https://open.spotify.com/track/7mVZeLt9f1IaYIl8lXgC59?si=af5963e36acc4840")],
                                                 [InlineKeyboardButton(text="Click", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D")]])
+date = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Yesterday", url='xxx.html')],
+    [InlineKeyboardButton(text="This Morning", url='xxx.html'),InlineKeyboardButton(text="This Evening", url='xxx.html')],
+    [InlineKeyboardButton(text="Tomorrow", url='xxx.html')]])
+
+priorities = ['HIGH','MID','LOW']
+
+async def pr():
+    keyboard = InlineKeyboardBuilder()
+    for i in priorities:
+        keyboard.add(InlineKeyboardButton(text=i, url="xx.html"))
+    return keyboard.adjust(1).as_markup()
